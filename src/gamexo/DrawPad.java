78f9a788e0e778f7a5a7f169acaf593c9f7163c9
ivanovIvan <depiy@gnail.pw>
2013-11-03 23:11:13 +0700

    Начало разработки

---
+++ src/gamexo/DrawPad.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamexo;
+import MyPakage.ClassXOImplement;
+
+/**
+ *
+ * @author dav
+ */
+public class DrawPad extend {
+    ClassXOImplement mObj;
+    
+    
+}
