f8fa24074a56f23070cf4dac24dd736a60a325e8
1 <1@Comp1>
2016-01-23 03:11:36 +0600

    work

--- src/dav/algo.java
+++ src/dav/algo.java
@@ -5,6 +5,7 @@
  */
 package dav;
 
+import Service.MyStdOut;
 import Sort.Sort_Insertion;
 import Sort.Sort_Merge_ExtendMemory;
 import Sort.Sort_Merge_NoMemory;
@@ -38,12 +39,15 @@ public class algo {
         UF_Test.Run(new UF_QuickFind(2000000),filePath);*/
        
        // SORT ARRAY
-       Integer[] testArray = Sort_Test.getTestArray(10);
+       //Integer[] testArray = Sort_Test.getTestArray(10);
+       Integer[] testArray = {314839861,730428587,154200775,634627186,341002217,510434320,257334558,133556827,465631549,708375567} ;
+       if (testArray.length<15) MyStdOut.printlnArray(testArray);
        /*Sort_Test.Run(Sort_Selection.class,testArray);
        Sort_Test.Run(Sort_Insertion.class,testArray);
        Sort_Test.Run(Sort_Shell.class,testArray);
        Sort_Test.Run(Etalon.Shell.class,testArray);*/
        //Sort_Merge_NoMemory.sort(testArray);
+       
        Sort_Test.Run(Sort_Merge_NoMemory.class,testArray);
        //Sort_Test.Run(Sort_Merge_ExtendMemory.class,testArray);
        //Sort_Merge_ExtendMemory.sort(testArray);
